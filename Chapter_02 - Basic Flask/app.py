ebd4a689447235bd0221df57ba14e9078b0d9e6b
Ommane123 <ommane1327@gmail.com>
2026-05-19 19:30:16 +0530

    Implement login functionality with session management and create welcome and logout routes

--- Chapter_02 - Basic Flask/app.py
+++ Chapter_02 - Basic Flask/app.py
@@ -1,5 +1,42 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, session, Response
 
 app = Flask(__name__)
+app.secret_key = "supersecret" 
 
-@app.route("/")
+#homepage or login page
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "admin" and password == "123":
+            session["user"] = username  # stores in session
+            return redirect(url_for("welcome"))
+        else:
+            return Response("Invalid credentials. try again...", mimetype="text/plain")
+        
+    return '''
+            <h2>Login Page</h2>
+            <form method = "POST">
+            Username: <input type = "text" name = "username"><br>
+            Password: <input type = "text" name = "password"><br>
+            <input type = "submit" value = "Login">
+            </form>
+'''
+
+# Welcome Page
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f'''
+        <h2>Welcome, {session["user"]}!</h2>
+        <a href = {url_for('logout')}>Logout</a>
+'''
+    return redirect(url_for("login"))  
+
+#logout
+@app.route("/logout")
+def logout():
+    session.pop("user", None)   # None = to prevent error if user is not present
+    return redirect(url_for("login"))
